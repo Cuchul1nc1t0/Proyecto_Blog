65db5a0513280718b33c259b70ea6826e5c90230
Leon Luna <lyon.raices@gmail.com>
2023-07-03 11:41:48 -0300

    Falta que tome el formulario como metodo 1°06'57"

--- project/apps/home/views.py
+++ project/apps/home/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from . import forms
 
 # Create your views here.
 def index(request):
     return render(request, 'home/index.html')
+
+def crear_autor(request):
+    if request.method == 'POST':
+        form = forms.AtorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'home/index.html')
+        else:
+            form = forms.Autor.Form()
+            context = {'form':form}
+            return render(request, 'home/crear_autor.html', context)
+
