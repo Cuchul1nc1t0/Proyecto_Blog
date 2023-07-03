84aceea4c6cc7b7b4db9046a4e4a40556aeab543
Leon Luna <lyon.raices@gmail.com>
2023-07-03 20:07:39 -0300

    App terminada sin Boostrap

--- project/apps/home/urls.py
+++ project/apps/home/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    #path('crear-post/', views.crear_post, name='crear-post'),
-    path('crear-autor/', views.crear_autor, name='crear-post'),
+    path('crear-post/', views.crear_post, name='crear-post'),
+    path('crear-autor/', views.crear_autor, name='crear-autor'),
 ]
