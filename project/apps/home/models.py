ad384f9951223dad6aadb5593248cfb8f1782988
Leon Luna <lyon.raices@gmail.com>
2023-07-02 21:11:30 -0300

    models en home post y autor.

--- project/apps/home/models.py
+++ project/apps/home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Posts(models.Model):
+    titulo = models.CharField(max_length=100)
+    contenido = models.TextField()
+
+    def __str__(self) -> str:
+        return self.titulo
+    
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.nombre
+    
